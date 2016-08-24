baf43b992cc225e3232f06dc1597da51221ab4f8
Jamie Brough <jamie1612@gmail.com>
2016-08-24 23:52:36 +0100

    add Procfile

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const apis = require('./apis');
 
 const app = require('./app')(apis());
-const PORT = process.env.SF_MONTAGE_PORT;
+const PORT = process.env.PORT;
 app.listen(PORT);
-console.log(`PB_RESIZER listening on ${PORT}`);
